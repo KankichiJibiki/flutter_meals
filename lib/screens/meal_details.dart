139e846a1f17a6a54ec4628142d28bef950b743d
kankichi.jibiki <kankichi.jibiki@sola-air.com>
2024-07-20 17:59:38 +0900

    all done!

---
+++ lib/screens/meal_details.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:meals/models/meal.dart';
+
+class MealDetailsScreen extends StatelessWidget {
+  const MealDetailsScreen({
+    super.key,
+    required this.meal,
+    required this.onToggleFavorite,
+  });
+
+  final Meal meal;
+  final void Function(Meal meal) onToggleFavorite;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(meal.title),
+        actions: [
+          IconButton(
+            onPressed: () {
+              onToggleFavorite(meal);
+            },
+            icon: const Icon(Icons.star),
+          )
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.network(
+              meal.imageUrl,
+              height: 250,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              'Ingredients',
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Column(
+              children: [
+                for (final ingredient in meal.ingredients)
+                  Text(
+                    ingredient,
+                    style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                          color: Theme.of(context).colorScheme.onBackground,
+                        ),
+                  )
+              ],
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              'Steps',
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Column(
+              children: [
+                for (final step in meal.steps)
+                  Text(
+                    step,
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                          color: Theme.of(context).colorScheme.onBackground,
+                        ),
+                  )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
